77f79b82954e4eb753b246b9214786aa17812d67
MarioHoyas98 <mariohoyas@gmail.com>
2022-12-28 13:01:34 +0100

    first commit

---
+++ src/components/TaskList.jsx
@@ -0,0 +1,25 @@
+import { useContext } from "react";
+import TaskCard from "./TaskCard";
+import { TaskContext } from "../context/TaskContext";
+
+function TaskList() {
+  const { tasks } = useContext(TaskContext);
+
+  if (tasks.length === 0) {
+    return (
+      <h1 className="text-white text-4xl font-bold text-center">
+        No hay tareas aún
+      </h1>
+    );
+  }
+
+  return (
+    <div className="grid grid-cols-4 gap-2">
+      {tasks.map((task) => (
+        <TaskCard task={task} key={task.id}></TaskCard>
+      ))}
+    </div>
+  );
+}
+
+export default TaskList;
